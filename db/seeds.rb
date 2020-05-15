ac5a9d852d12a4d52ef3ce3c77f047f40aa482cf
Myriam Dubois <myriam.dubois@skema.edu>
2020-05-15 12:36:30 +0200

    routes done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'json'
+require 'open-uri'
+
+url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+ingredient_list = open(url).read
+ingredient = JSON.parse(ingredient_list)
+
+ingredient["drinks"].each do |ingredient|
+  Ingredient.create!(name: ingredient["strIngredient1"])
+end
